6934b59453722d805185fd84d668c4fe696e861d
longtaoliu <ge96rox@mytum.de>
2021-06-10 17:35:13 +0200

    implement task1

---
+++ task1.py
@@ -0,0 +1,30 @@
+import sys
+import numpy as np
+from scipy.spatial.distance import cdist
+
+
+def approx_nonlinear_func(x,fx,L,e):
+    """
+    Approximates a function using nonlinear radial functions as a basis.
+
+    Parameters
+    ----------
+   
+    Returns
+    -------
+   
+    """
+   
+    # choose L random elements of the data
+    rng = np.random.default_rng()
+    x_l = rng.choice(x,L)
+      
+    # choose epsilon similar to diffusion map
+    dist = cdist(x,x_l)
+    epsilon = e * np.max(dist)
+    phi_l = np.exp(-dist**2/epsilon**2)
+    
+    C,residues,_,_ = np.linalg.lstsq(phi_l, fx, rcond=None)
+    
+    
+    return C,residues,epsilon,x_l
